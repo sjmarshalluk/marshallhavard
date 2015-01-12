db8f1d00436310ef6cebded545e9feb8aa7517d1
Steve Marshall <steve@stevemarshalldesign.co.uk>
2015-01-12 15:59:02 +0000

    Added user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :guides
   resources :questions
   resources :services
+  resources :users
 
   root "guides#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112104946) do
+ActiveRecord::Schema.define(version: 20150112154418) do
 
   create_table "guides", force: true do |t|
     t.string   "title"
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 20150112104946) do
     t.boolean  "overseas"
     t.boolean  "particular_tax_advice"
     t.boolean  "employed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
