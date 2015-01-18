f3ca6f46ad67982dc107446ac55cd3c47a9ce502
Steve Marshall <steve@stevemarshalldesign.co.uk>
2015-01-18 17:58:51 +0000

    something else for tinymce

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.precompile += %w[tinymce/tinymce.min.js tinymce/langs/en.js tinymce/themes/advanced/editor_template.js]
+  config.assets.precompile += %w[tinymce/tinymce.min.js]
 end
