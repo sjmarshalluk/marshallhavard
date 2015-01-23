c2eef3ccfb65f05f53d6b4039e016dfb485c2c49
Steve Marshall <steve@stevemarshalldesign.co.uk>
2015-01-23 12:30:43 +0000

    specifying all css and js in precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.precompile += %w[tinymce/tinymce.min.js]
+  config.assets.precompile += %w( *.css *.js )
 end
